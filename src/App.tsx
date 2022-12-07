71f9a9a882baa0b15ffa194cdcedb85f5e12317e
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-07 17:18:54 +0400

    adding styled-component & @types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { SInput } from "./components/SInput";
+
 const App = () => {
   return (
     <div className="App">
       <h1>hello, world</h1>
+      <SInput />
     </div>
   );
 };
---
+++ src/components/SInput/SInput.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const SInput = styled.input`
+  width: 100%;
+  height: 3rem;
+  border: 1px solid #111;
+  outline: none;
+  border-radius: 3px;
+  font-size: 1.1rem;
+`;
