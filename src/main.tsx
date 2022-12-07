aaef30ac871eb69be03794daab8689d86147b518
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-07 20:29:44 +0400

    adding create context hook

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./config";
+import { ValidationContext } from "./Context/ValidationContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ValidationContext>
+      <GlobalStyle />
+      <App />
+    </ValidationContext>
   </React.StrictMode>
 );
