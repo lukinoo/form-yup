a6e976214eac2924b6b00f96a0ea0e754fc367bc
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-08 16:07:41 +0400

    adding assets folder with js svg logo

--- src/view/RegistrationForm/RegistrationForm.styled.ts
+++ src/view/RegistrationForm/RegistrationForm.styled.ts
@@ -13,8 +13,10 @@ export const SForm = styled.form`
     margin: 0.5rem 0;
     &:nth-child(1) {
       position: relative;
-      svg {
+      img {
         position: absolute;
+        width: 50px;
+        height: 50px;
         top: 0;
         right: 0;
       }
