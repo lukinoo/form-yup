5af9656cd876fa28517d27a9fcc540d0de2678db
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-10 19:00:11 +0400

    📦NEW: adding complete route on submit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { GlobalStyle } from "./config";
 import { ValidationContext } from "./Context/ValidationContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ValidationContext>
-      <GlobalStyle />
-      <App />
-    </ValidationContext>
+    <BrowserRouter>
+      <ValidationContext>
+        <GlobalStyle />
+        <App />
+      </ValidationContext>
+    </BrowserRouter>
   </React.StrictMode>
 );
