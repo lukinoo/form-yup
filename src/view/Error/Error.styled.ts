7b623f57225a88f3417ab4c58554bd0b68b0facc
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-12 19:33:05 +0400

    📦NEW: adding Error component

---
+++ src/view/Error/Error.styled.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const SError = styled.h4`
+  font-size: 2.5rem;
+  color: #fff;
+`;
+
+export const SErrorBackButton = styled.button`
+  border: 1px solid #fff;
+  height: 2.5rem;
+  outline: none;
+  background: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0 0.5rem;
+  padding: 0.5rem 1rem;
+  margin: 1rem 0 0 0;
+  border-radius: 4px;
+  color: #ffffff;
+  font-family: inherit;
+  text-transform: uppercase;
+  font-size: 0.9rem;
+  font-weight: 400;
+  letter-spacing: 0.3px;
+  cursor: pointer;
+`;
