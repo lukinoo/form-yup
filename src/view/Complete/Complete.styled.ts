007ed3112463af4a77b7ff6426e68c24fd49968b
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-12 19:20:15 +0400

    📦NEW: adding Complete route & user check logic

---
+++ src/view/Complete/Complete.styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const SComplete = styled.div`
+  max-width: 600px;
+  background: #ffffff;
+  padding: 1.5rem 1.8rem;
+  border-radius: 8px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 0.5rem;
+  }
+  p {
+    font-size: 1.1rem;
+  }
+`;
